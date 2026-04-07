335dcbc2fbbcb0d0f72a5324507d33606a837320
Jasur <uktamovjasur137@gmail.com>
2026-04-07 15:30:09 +0500

    done

--- task01.py
+++ task01.py
@@ -1,1 +1,11 @@
+people = [("Ali", 24), ("Laylo", 30), ("Jasur", 19)]
 
+max_age = 0
+max_person = ("")
+
+for name, age in people:
+    if age > max_age:
+        max_age = age
+        max_person = name
+
+print(f"{max_person} -  {max_age}")
