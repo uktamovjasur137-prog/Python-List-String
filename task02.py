83003ebd9439c122e5f3409ce159377475cc0dfe
Jasur <uktamovjasur137@gmail.com>
2026-04-07 15:39:54 +0500

    done

--- task02.py
+++ task02.py
@@ -1,1 +1,10 @@
+students = [("Ali", ["Fizika", "Matematika"]), ("Laylo", ["Ingliz tili"]), ("Jasur", ["Matematika", "Informatika"])]
 
+choosen = ()
+
+count = 0
+for name, subject in students:
+    if "Matematika" in subject:
+        count += 1
+
+print(f"{count} talaba matematikani tanlagan")
