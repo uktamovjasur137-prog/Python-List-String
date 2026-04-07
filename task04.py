fe9a02b8985563e8adcd93f38b57f3a3a0a5b481
Jasur <uktamovjasur137@gmail.com>
2026-04-07 15:45:44 +0500

    done

--- task04.py
+++ task04.py
@@ -1,1 +1,9 @@
+orders = [(102, "Ali"), (99, "Vali"), (150, "Sami")]
 
+juft_sonlari = ()
+
+for num, name in orders:
+    if num % 2 == 0:
+        juft_sonlari += ((num, name),)
+
+print(juft_sonlari)
